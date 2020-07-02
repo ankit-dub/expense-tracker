7ebe65946e35567c559f2bd4ed59f0296ad6be4d
Ankit <dankit.773@gmail.com>
2020-07-02 18:43:21 +0530

    commit

--- Array.java
+++ Array.java
@@ -1,8 +1,9 @@
+import java.io.OutputStream;
 import java.util.*;
 class Array{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Vector foo = new Vector();
+        ArrayList foo = new ArrayList();
         for (int i = 1; i <= 5; i++) 
             foo.add(sc.nextInt());
         System.out.println(foo);
@@ -11,10 +12,15 @@ class Array{
         // Displaying the List 
         // after deletion 
         System.out.println(foo); 
-  
+        
         // Printing elements one by one 
         for (int i = 0; i < foo.size(); i++) 
             System.out.print(foo.get(i) + " ");
+        System.out.println();
+        Iterator it = foo.iterator();
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
     }
         
     
