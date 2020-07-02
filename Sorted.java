7a2a4a2b58de8a1f1ba31bdac2c708a5adc25b46
Ankit Dubey <ankit@moneytap.com>
2020-07-02 19:31:00 +0000

    Update Sorted.java

--- Sorted.java
+++ Sorted.java
@@ -8,15 +8,8 @@ class Sorted{
         loans.add(new Loan(10000.0,8,"Monish"));
         loans.add(new Loan(10000.0,6,"Sajal"));
        //Collections.sort(loans);
-       for(Loan l:loans){
-           System.out.println(l.loanAmount+" "+l.tenure+" "+l.loanApplicantName);}
-    //    loanAmountSort amountSort=new loanAmountSort();
-    //    Collections.sort(loans,amountSort);
-    //    for(Loan n:loans){
-    //        System.out.println(n.loanAmount+" "+n.tenure+" "+n.loanApplicantName);
-    //        if(n.loanApplicantName.startsWith("A"))
-    //        System.out.println("Get loan with : "+n.loanApplicantName);
-    //    }
-       
+       for(Loan l_set:loans){
+           System.out.println(l_set.loanAmount+" "+l_set.tenure+" "+l_set.loanApplicantName);}
+    
    }
 }
