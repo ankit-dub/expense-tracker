d565a96f5cd1a4eb5c6a6a3b25a28ec3841d2a4c
Ankit <dankit.773@gmail.com>
2020-07-04 01:05:13 +0530

    do it

--- Sorted.java
+++ Sorted.java
@@ -7,16 +7,10 @@ class Sorted{
         loans.add(new Loan(16000.0,4,"Sandesh"));
         loans.add(new Loan(10000.0,8,"Monish"));
         loans.add(new Loan(10000.0,6,"Sajal"));
-       //Collections.sort(loans);
+       
        for(Loan l:loans){
-           System.out.println(l.loanAmount+" "+l.tenure+" "+l.loanApplicantName);}
-    //    loanAmountSort amountSort=new loanAmountSort();
-    //    Collections.sort(loans,amountSort);
-    //    for(Loan n:loans){
-    //        System.out.println(n.loanAmount+" "+n.tenure+" "+n.loanApplicantName);
-    //        if(n.loanApplicantName.startsWith("A"))
-    //        System.out.println("Get loan with : "+n.loanApplicantName);
-    //    }
+           System.out.println(l.loanAmount+" "+l.tenure+" "+l.loanApplicantName);
+        }
        
    }
 }
