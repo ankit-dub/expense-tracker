b8b9aedcfc18c9c10f640dd7457fdf6ba9741db6
Ankit <dankit.773@gmail.com>
2020-07-08 12:59:59 +0530

    Authorisation

--- security/src/main/java/com/moneytap/ankit/AppSecurity.java
+++ security/src/main/java/com/moneytap/ankit/AppSecurity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -28,6 +29,15 @@ public class AppSecurity extends WebSecurityConfigurerAdapter{
 
         
         return new InMemoryUserDetailsManager(Users);
+
     }
     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/topics").hasAnyRole("IT","admin")
+        .antMatchers("/hello").hasAnyRole("admin","electrical")
+        .antMatchers("/").permitAll()
+        .and().formLogin();
+    }
 }
